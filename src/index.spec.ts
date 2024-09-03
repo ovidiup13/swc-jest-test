c4747aad2fe31b6738c8f6a609a891dd7ef43af6
Ovidiu Popoviciu <devovip13@gmail.com>
2024-09-03 15:48:11 +0100

    chore: add wildcard test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,14 @@
 import { OBJECT } from './index';
+import * as m from './index';
 
 const spy = jest.spyOn(OBJECT, 'sum');
+const wildcardSpy = jest.spyOn(m, 'sum');
 
 describe('test', () => {
   it('should call the sum function', () => {
     const result = OBJECT.sum(1, 2);
 
     expect(spy).toHaveBeenCalled();
+    expect(wildcardSpy).toHaveBeenCalled();
   });
 });
